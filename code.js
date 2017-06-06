8abea89831e9656e9ed6a4c4414f33c138ec940a
Anthony Smith <anthonysmith1057@gmail.com>
2017-06-06 21:26:19 +0100

    checks contact us page for information

--- code.js
+++ code.js
@@ -1,56 +1,26 @@
 const methods = require('./methods.js');
-const cheerio = require('cheerio');
-const Knwl = require("knwl.js");
-const knwlInstance = new Knwl('english');
-const request = require('request');
 
 try {
 	
 	//get email address from arguments
-	var emailAddress = methods.getEmail(process.argv[2]); 
+	var emailAddress = methods.getEmail(process.argv[2]);
 	//get information from email.
-
 	var website = methods.getWebsite(emailAddress);
-	//loop until variable update
+	//get info from website and contact page
+	methods.scrapeWebsite(website); 
 
-	request(website, function(error, response, html) {
+	var getEmails = function() {
 
-		if (error) {
+		console.log("\n");
+		console.log(methods.getEmails());
+		console.log(methods.getPhones());
+		console.log(methods.getPlaces());
+		console.log(methods.getLinks());
+		console.log(methods.getTimes());
+		console.log(methods.getDates());
 
-			throw error; 
-
-		}
-
-		//get relevent information 
-		var $ = cheerio.load(html);
-
-		var body = $("body").html(); 
-
-		knwlInstance.init(body); 
-
-		console.log(knwlInstance.get('emails'));
-
-		//find contact us link 
-
-		var contactPage = methods.getContactUsLink($, website);
-
-		//open link
-		request(contactPage, function(error, response, html) {
-
-			$ = cheerio.load(html);
-
-			body = $("body").html(); 
-			
-			knwlInstance.init(body); 
-			console.log(knwlInstance.get('emails'));
-
-		}); 
-
-	});
-	
-	 
-	
-	
+	}
+	setInterval(function() {getEmails()},1000);
 
 } catch (error) {
 
--- methods.js
+++ methods.js
@@ -1,4 +1,114 @@
+const request = require('request');
+const Knwl = require("knwl.js");
+const knwlInstance = new Knwl('english');
+const cheerio = require('cheerio');
+var a_emails = []; 
+var a_phones = [];
+var a_places = [];
+var a_links = []; 
+var a_times = []; 
+var a_dates = []; 
+var $; 
+
+function getInfoFromHtml(html) {
+
+	$ = cheerio.load(html);
+
+	var body = $('body').html();
+	knwlInstance.init(body);
+	var emails = knwlInstance.get('emails');
+
+	a_emails.push(emails);
+	knwlInstance.init(body);
+	var phones = knwlInstance.get('phones');
+
+	a_phones.push(phones);
+	knwlInstance.init(body);
+	var places = knwlInstance.get('places');
+
+	a_places.push(places);
+	knwlInstance.init(body);
+	var links = knwlInstance.get('links');
+	
+	a_links.push(links);
+	knwlInstance.init(body);
+	var times = knwlInstance.get('times');
+	
+	a_times.push(times);
+	knwlInstance.init(body);
+	var dates = knwlInstance.get('dates');
+	
+	a_dates.push(dates);
+	
+	
+
+}
+function getContactUsLink($, website) {
+
+	var body = $("body").html();
+	var a = $("a");
+	for (var i = 0; i < a.length; i++) {
+
+		if (new RegExp(".*contact.*").test(a[i].attribs.href.toLowerCase())) {
+
+			var contact = a[i].attribs.href;
+
+			if (new RegExp("^https?://.+$").test(contact)) {
+
+				return contact;
+
+			} else {
+
+				if (!new RegExp("^\\/.+$").test(contact)) {
+
+					contact = "/"+contact; 
+
+				}
+
+				return website + contact; 
+
+			}
+
+		}
+
+	}
+
+	return null; 
+
+}
+
 module.exports = {
+
+	getEmails: function() {
+
+		return a_emails; 
+
+	},
+	getPhones: function() {
+
+		return a_phones; 
+
+	},
+	getPlaces: function() {
+
+		return a_places; 
+
+	},
+	getLinks: function() {
+
+		return a_links; 
+
+	},
+	getTimes: function() {
+
+		return a_times; 
+
+	},
+	getDates: function() {
+
+		return a_dates; 
+
+	},
 
 	//get email from arguments. return email address as a string
 	getEmail: function(args) {
@@ -30,34 +140,38 @@ module.exports = {
 
 	},
 
-	getContactUsLink: function($, website) {
+	scrapeWebsite: function(url) {
 
-		var body = $("body").html();
-		var a = $("a");
-		for (var i = 0; i < a.length; i++) {
+		request(url, function(error, response, html) {
 
-			if (new RegExp(".*contact.*").test(a[i].attribs.href.toLowerCase())) {
+			if (error) {
 
-				var contact = a[i].attribs.href;
-
-				if (new RegExp("^https?://.+$").test(contact)) {
-
-					return contact;
-
-				} else {
-
-					if (!new RegExp("^\\/.+$").test(contact)) {
-
-						contact = "/"+contact; 
-
-					}
-
-					return website + contact; 
-
-				}
+				throw error; 
 
 			}
 
-		}
+			//get relevent information 
+			getInfoFromHtml(html);
+
+			//try contact us page
+			var contactUs = getContactUsLink($,url);
+			if (contactUs != null) {
+
+				request(contactUs, function(error, response, html) {
+
+					if (error) {
+
+						throw error; 
+
+					}
+
+					getInfoFromHtml(html);
+
+				});
+
+			}
+
+		});
+
 	}
 }
